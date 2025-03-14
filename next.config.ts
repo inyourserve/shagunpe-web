6795058d5f584c5447acecbe250d72fc265f0983
Vikash Kumar <inyourserve@gmail.com>
2025-03-14 17:13:45 +0530

    feature section improve

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,45 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+// /** @type {import('next').NextConfig} */
+// module.exports = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "shagunpe.in",
+//         pathname: "/asset/**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "bloggervikash.com",
+//         pathname: "/wp-content/uploads/**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "img.freepik.com",
+//         pathname: "/**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "api.dicebear.com",
+//         pathname: "/**",
+//       },
+      
+//       {
+//         protocol: "https",
+//         hostname: "sjc.microlink.io",
+//         pathname: "/**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "images.unsplash.com",
+//         pathname: "/photo-**",
+//       },
+//     ],
+//   },
+// }
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
@@ -22,7 +62,6 @@ module.exports = {
         hostname: "api.dicebear.com",
         pathname: "/**",
       },
-      
       {
         protocol: "https",
         hostname: "sjc.microlink.io",
@@ -35,5 +74,9 @@ module.exports = {
       },
     ],
   },
-}
+  env: {
+    VAR_ORIGINAL_PATHNAME: process.env.VAR_ORIGINAL_PATHNAME || "/", // ✅ Fix missing variable issue
+  },
+};
 
+export default nextConfig;
